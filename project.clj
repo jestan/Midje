f17b8e239214e761ead1278cf8c2fbd1cb599ed0
Jestan Nirojan <jestannirojan@gmail.com>
2011-10-17 18:55:04 +0530

    1)set clojure 1.3.0 as default 2)excluded old clojure versions in swank deps

--- project.clj
+++ project.clj
@@ -1,11 +1,12 @@
-(defproject midje "1.3-alpha4"
+(defproject org.clojars.hms/midje "1.3-alpha4"
   :description "A TDD library for Clojure, with an emphasis on mocks"
-  :repositories {"sonatype-oss-public" "https://oss.sonatype.org/content/groups/public/"}
-  :dependencies [[org.clojure/clojure "[1.2.0],[1.2.1],[1.3.0]"]
-;                 [ordered "0.3.0"]  ; Not yet updated for Clojure 1.3.
+  :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/math.combinatorics "0.0.1"]
                  [org.clojure/algo.monads "0.1.0"]
                  [org.clojure/core.unify "0.5.1"]]
-  :dev-dependencies [[swank-clojure "1.2.1"]
-                     [slamhound "1.1.1"]])
+  :dev-dependencies [[swank-clojure "1.3.2" :exclusions [org.clojure/clojure]]
+                     [slamhound "1.1.1"]]
+
+  :repositories {"releases" "http://192.168.0.7:8080/archiva/repository/internal/"
+                 "snapshots" "http://192.168.0.7:8080/archiva/repository/snapshots/"})
 
